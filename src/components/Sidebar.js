35f624dd6e3596ffd119d524f7c6b63cc47c1e8f
rajshah1606 <raj.shah@budhanatech.com>
2023-07-04 16:01:47 +0530

    Optimized recursively

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,20 +1,54 @@
-import React, { useState } from 'react'
-import "../styles/sidebar.css"
+import React, { useState } from 'react';
+import "../styles/sidebar.css";
 import Accordion from '@mui/material/Accordion';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import QueryStatsIcon from '@mui/icons-material/QueryStats';
-import { data } from '../constants';
-import CheckComponent from '../ui/CheckComponent';
+import { data, panelClick } from '../constants';
 import Summary from '../ui/Summary';
+
 const Sidebar = () => {
     const [expandedPanels, setExpandedPanels] = useState([]);
-    const [childPanles, setChildPanels] = useState([])
-    const [innerChildPanels, setInnerChildPanels] = useState([])
+
     const handlePanelClick = (panelIndex, state, setState) => {
-        const newExpandedPanels = state.includes(panelIndex)
-            ? state.filter((index) => index !== panelIndex)
-            : [...state, panelIndex];
+        const newExpandedPanels = panelClick(panelIndex, state, setState)
         setState(newExpandedPanels);
+    };
+
+    const renderAccordion = (dataArray, state, setState) => {
+        return dataArray.map((sampledata, index) => (
+            <div key={index} className='side_below'>
+                <div>
+                    <Accordion
+                        expanded={state.includes(index)}
+                        onChange={() => handlePanelClick(index, state, setState)}
+                        style={{
+                            background: state.includes(index) ? "#4BA3DF" : "#070927",
+                            color: "white",
+                            boxShadow: 'none',
+                            borderRadius: "0",
+                            borderBottom: state.includes(index) ? "1px solid #3F445D" : "1px solid #121337",
+                        }}
+                    >
+                        <Summary data={sampledata} index={index} />
+                        <AccordionDetails style={{ background: "#0F1332" }}>
+                            <div className='expand'>
+                                <div className='flexCol'>
+                                    {sampledata?.nodes?.length > 0 && sampledata?.nodes?.map((innerData, innerIndex) => (
+                                        <RecursiveAccordion
+                                            key={innerIndex}
+                                            data={innerData}
+                                            index={innerIndex}
+                                            state={state}
+                                            setState={setState}
+                                        />
+                                    ))}
+                                </div>
+                            </div>
+                        </AccordionDetails>
+                    </Accordion>
+                </div>
+            </div>
+        ));
     };
 
     return (
@@ -25,88 +59,52 @@ const Sidebar = () => {
                     <h4>Statistics</h4>
                 </div>
             </div>
-            {data?.map((sampledata, index) => (
-                <div key={index} className='side_below'>
-                    <div>
-                        <Accordion
-                            expanded={expandedPanels.includes(index)}
-                            onChange={() => handlePanelClick(index, expandedPanels, setExpandedPanels)
-                            }
-                            style={{
-                                background: expandedPanels.includes(index) ? "#4BA3DF" : "#070927", color: "white", boxShadow: 'none', borderRadius: "0", borderBottom: expandedPanels.includes(index) ? "1px solid #3F445D" : "1px solid #121337",
-                            }}
-                        >
-                            <Summary data={sampledata} index={index} />
-                            <AccordionDetails style={{ background: "#0F1332" }}>
-                                <div className='expand'>
-                                    <div className='flexCol'>
-                                        {sampledata.nodes[0].nodes[0]?.nodes?.length === 0
-                                            ? sampledata.nodes[0].nodes.map((innerData, innerIndex) => (
-                                                <CheckComponent data={innerData} index={innerIndex} />
-                                            ))
-                                            : sampledata.nodes[0]?.nodes?.map((data, innerInd) => (
-                                                <Accordion
-                                                    expanded={childPanles.includes(innerInd)}
-                                                    onChange={() => handlePanelClick(innerInd, childPanles, setChildPanels)}
-                                                    style={{
-                                                        background: expandedPanels.includes(innerInd) ? "#0F1332" : "#0F1332",
-                                                        color: "white",
-                                                        boxShadow: 'none',
-                                                        borderRadius: "0",
-                                                    }}
-                                                >
-                                                    <Summary data={data} index={innerInd} />
-                                                    <AccordionDetails>
-                                                        <div className='expand'>
-                                                            <div className='flexCol'>
-                                                                {data.nodes.length === 0 ? null : (
-                                                                    <>
-                                                                        {data.nodes.map((data, index) => (
-                                                                            <Accordion
-                                                                                expanded={innerChildPanels.includes(index)}
-                                                                                onChange={() => handlePanelClick(index, innerChildPanels, setInnerChildPanels)}
-                                                                                style={{
-                                                                                    background: innerChildPanels.includes(index) ? "#0F1332" : "#0F1332",
-                                                                                    color: "white",
-                                                                                    boxShadow: 'none',
-                                                                                    borderRadius: "0",
-                                                                                }}
-                                                                            >
-                                                                                <Summary data={data} index={index} />
-                                                                                <AccordionDetails>
-                                                                                    <div className='expand'>
-                                                                                        <div className='flexCol'>
-                                                                                            {data.nodes.length === 0 ? null : (
-                                                                                                <>
-                                                                                                    {data.nodes.map((data, index) => (
-                                                                                                        <CheckComponent index={index} data={data} />
-                                                                                                    ))}
-                                                                                                </>
-                                                                                            )}
-                                                                                        </div>
-                                                                                    </div>
-                                                                                </AccordionDetails>
-                                                                            </Accordion>
-                                                                        ))}
-                                                                    </>
-                                                                )}
-                                                            </div>
-                                                        </div>
-                                                    </AccordionDetails>
-                                                </Accordion>
-                                            ))
-                                        }
-                                    </div>
-                                </div>
-                            </AccordionDetails>
-                        </Accordion>
+            {renderAccordion(data, expandedPanels, setExpandedPanels)}
+        </div>
+    );
+};
+
+const RecursiveAccordion = ({ data, index, state, setState }) => {
+    const [childPanels, setChildPanels] = useState([]);
+
+    const handlePanelClick = (panelIndex, childPanels, setChildPanels) => {
+        const newExpandedPanels = panelClick(panelIndex, childPanels, setChildPanels)
+        setChildPanels(newExpandedPanels);
+    };
+
+    return (
+        <Accordion
+            expanded={childPanels.includes(index)}
+            onChange={() => {
+                if (data?.nodes?.length > 0) {
+                    handlePanelClick(index, childPanels, setChildPanels);
+                }
+            }}
+            style={{
+                background: state.includes(index) ? "#0F1332" : "#0F1332",
+                color: "white",
+                boxShadow: 'none',
+                borderRadius: "0",
+            }}
+        >
+            <Summary data={data} index={index} />
+            <AccordionDetails>
+                <div className='expand'>
+                    <div className='flexCol'>
+                        {data?.nodes?.length > 0 && data?.nodes?.map((nestedData, nestedIndex) => (
+                            <RecursiveAccordion
+                                key={nestedIndex}
+                                data={nestedData}
+                                index={nestedIndex}
+                                state={state}
+                                setState={setState}
+                            />
+                        ))}
                     </div>
                 </div>
-            ))}
-        </div>
-    )
-}
+            </AccordionDetails>
+        </Accordion>
+    );
+};
 
-export default Sidebar
-
-
+export default Sidebar;
--- src/constants.js
+++ src/constants.js
@@ -4,30 +4,25 @@ export const data = [
         label: "Point Of Interests",
         nodes: [
             {
-                key: "poi",
-                label: "poi",
-                nodes: [
-                    {
-                        key: "district",
-                        label: "District",
-                        nodes: [],
-                        url: "https://www.google.com/search?q=dog"
-                    },
-                    {
-                        key: "schools",
-                        label: "schools",
-                        nodes: [],
-                        url: "https://www.google.com/search?q=fox"
-                    },
-                    {
-                        key: "cp",
-                        label: "Capex Projects",
-                        nodes: [],
-                        url: "https://www.google.com/search?q=wolf"
-                    }
-                ],
-                url: "https://www.google.com/search?q=canidae"
+                key: "district",
+                label: "District",
+                nodes: [],
+                url: "https://www.google.com/search?q=dog"
+            },
+            {
+                key: "schools",
+                label: "schools",
+                nodes: [],
+                url: "https://www.google.com/search?q=fox"
+            },
+            {
+                key: "cp",
+                label: "Capex Projects",
+                nodes: [],
+                url: "https://www.google.com/search?q=wolf"
             }
+
+
         ],
         url: "https://www.google.com/search?q=mammal"
     },
@@ -36,51 +31,58 @@ export const data = [
         label: "Challenges",
         nodes: [
             {
-                key: "challenge",
-                label: "challenge",
+                key: "2022",
+                label: "2022",
                 nodes: [
                     {
-                        key: "2022",
-                        label: "2022",
+                        key: "highways",
+                        label: "Highways",
                         nodes: [
                             {
-                                key: "highways",
-                                label: "Highways",
-                                nodes: [
-                                    {
-                                        key: "morning",
-                                        label: "Morning Congestion",
-                                    },
-                                    {
-                                        key: "morning",
-                                        label: "Morning Congestion",
-                                    }
-                                ]
-                            },
-                            {
-                                key: "junctions",
-                                label: "Junctions",
+                                key: "morning",
+                                label: "Morning Congestion",
                                 nodes: []
                             },
                             {
-                                key: "bridges",
-                                label: "Bridges",
+                                key: "morning",
+                                label: "Morning Congestion",
                                 nodes: []
-                            },
-
-                        ],
-                        url: "https://www.google.com/search?q=dog"
+                            }
+                        ]
                     },
                     {
-                        key: "2025",
-                        label: "2025",
-                        nodes: [],
-                        url: "https://www.google.com/search?q=fox"
+                        key: "junctions",
+                        label: "Junctions",
+                        nodes: []
                     },
+                    {
+                        key: "bridges",
+                        label: "Bridges",
+                        nodes: []
+                    },
+
                 ],
-                url: "https://www.google.com/search?q=canidae"
-            }
+                url: "https://www.google.com/search?q=dog"
+            },
+            {
+                key: "2025",
+                label: "2025",
+                nodes: [],
+                url: "https://www.google.com/search?q=fox"
+            },
+
+
         ],
         url: "https://www.google.com/search?q=mammal"
     },
 ]
+
+
+
+export const panelClick = (panelIndex, state, setState) => {
+    const newExpandedPanels = state.includes(panelIndex)
+        ? state.filter((index) => index !== panelIndex)
+        : [...state, panelIndex];
+
+    return newExpandedPanels
+}
